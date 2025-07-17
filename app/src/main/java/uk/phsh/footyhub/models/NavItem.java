31eaca4ddc38ee29e8fb76ebec5b211b3779e65b
Peter Blackburn <professorpicard@proton.me>
2025-07-17 22:21:50 +0100

    Allowed gridview to span all available space on SelectTeamFrag Fixed a bug that was resetting the current fragment to the home page when changing the configuration (rotating or selecting dark mode)

--- app/src/main/java/uk/phsh/footyhub/models/NavItem.java
+++ app/src/main/java/uk/phsh/footyhub/models/NavItem.java
@@ -1,13 +1,15 @@
 package uk.phsh.footyhub.models;
 
+import androidx.fragment.app.Fragment;
+
 import uk.phsh.footyhub.fragments.BaseFragment;
 
 public class NavItem {
     private String _title;
     private int _icon;
-    private BaseFragment _fragment;
+    private Fragment _fragment;
 
-    public NavItem(String title, int icon, BaseFragment fragment) {
+    public NavItem(String title, int icon, Fragment fragment) {
         this._title = title;
         this._icon = icon;
         this._fragment = fragment;
@@ -15,7 +17,7 @@ public class NavItem {
 
     public int getIcon() { return _icon; }
     public String getTitle() { return _title; }
-    public BaseFragment getFragment() { return _fragment; }
+    public Fragment getFragment() { return _fragment; }
 
     public void setTitle(String value) { this._title = value; }
     public void setIcon(int value) { this._icon = value; }
